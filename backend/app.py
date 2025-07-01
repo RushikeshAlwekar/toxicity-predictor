9379ed29c88e047a255daa9f52abb67f5d654cd8
RushikeshAlwekar <alwekarrushikesh@gmail.com>
2025-07-01 19:14:07 +0530

    Added render config

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 from model.preprocessing import clean_text
+import os
+
+port = int(os.environ.get("PORT", 10000))
+
+app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
